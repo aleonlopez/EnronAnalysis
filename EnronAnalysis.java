9bbe24aa75aca954b4d7d0386b8679c93836d4bb
aleonlopez <112011593+aleonlopez@users.noreply.github.com>
2023-05-22 13:33:24 -0700

    Update EnronAnalysis.java

--- EnronAnalysis.java
+++ EnronAnalysis.java
@@ -1,1 +1,33 @@
+import java.util.*;
 
+public class EnronAnalysis {
+    public static void main(String[] args) {
+        String path = args[0];
+        String outPath = args[1];
+
+        if (path.isEmpty()) {
+            System.out.println("No path found for first input.");
+        }
+
+        Reader reader = new Reader(path);
+
+        Connectors connectors = new Connectors(reader.getPeople(), reader.getEdges(),outPath);
+        // call to find and print connectors
+        connectors.findConnectors();
+        connectors.printConnectors();
+        // getting user input
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("");
+            System.out.print("Email address of the individual (or EXIT to quit): ");
+            String email = scanner.nextLine().trim();
+            // if "EXIT" then
+            if (email.equalsIgnoreCase("EXIT")) {
+                break;
+            }
+            // call to print the details of the email requested
+            connectors.printPersonDetails(email);
+        }
+        scanner.close();
+    }
+}
