8aee8ec8e7d9509c8aee37e7aefe1aee4ad06555
aleonlopez <112011593+aleonlopez@users.noreply.github.com>
2023-05-22 18:09:01 -0700

    added documentation to Connectors.java

--- Connectors.java
+++ Connectors.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.*;
 
+/**
+ * The Connectors class finds connectors in a graph represented by a set of people and edges.
+ * It also provides methods to print the connectors, print person details, and perform graph operations.
+ */
 public class Connectors {
     private Set<String> people;
     private Set<String[]> edges;
@@ -8,6 +12,13 @@ public class Connectors {
     private String outputFile;
     private Map<String, Set<String>> graph;
 
+    /**
+     * Constructs a Connectors object with the specified people, edges, and output file.
+     *
+     * @param people      the set of people in the graph
+     * @param edges       the set of edges in the graph
+     * @param outputFile  the path of the output file to write connectors to
+     */
     public Connectors(Set<String> people, Set<String[]> edges, String outputFile) {
         this.people = people;
         this.edges = edges;
@@ -17,6 +28,9 @@ public class Connectors {
         buildGraph();
     }
 
+    /**
+     * Builds the graph representation using the set of edges.
+     */
     private void buildGraph() {
         for (String[] edge : edges) {
             String person1 = edge[0];
@@ -30,6 +44,9 @@ public class Connectors {
         }
     }
 
+    /**
+     * Finds the connectors in the graph.
+     */
     public void findConnectors() {
         for (String person : people) {
             Set<String> visited = new HashSet<>();
@@ -40,6 +57,15 @@ public class Connectors {
         }
     }
 
+    /**
+     * Performs a depth-first search (DFS) on the graph to find connectors.
+     *
+     * @param vertex   the current vertex being visited
+     * @param parent   the parent of the current vertex
+     * @param visited  the set of visited vertices
+     * @param dfsNum   a map storing the DFS number of each vertex
+     * @param back     a map storing the back number of each vertex
+     */
     private void dfs(String vertex, String parent, Set<String> visited, Map<String, Integer> dfsNum, Map<String, Integer> back) {
         visited.add(vertex);
         dfsNum.put(vertex, dfsNum.size() + 1);
@@ -55,7 +81,7 @@ public class Connectors {
 
             if (!visited.contains(neighbor)) {
                 children++;
-                dfs(neighbor, vertex, visited, dfsNum, back);
+                dfs(neighbor, vertex, visited, dfsNum, back); // Recursive call for dfs
 
                 if (dfsNum.get(vertex) <= back.get(neighbor)) {
                     isConnector = true;
@@ -68,10 +94,13 @@ public class Connectors {
         }
 
         if ((parent.equals(vertex) && children > 1) || (!parent.equals(vertex) && isConnector)) {
-            connectors.add(vertex);
+            connectors.add(vertex); // Adding to the list of connectors
         }
     }
 
+    /**
+     * Prints the connectors to the console and writes them to the output file if specified.
+     */
     public void printConnectors() {
         Set<String> uniqueConnectors = new HashSet<>(connectors);
 
@@ -92,6 +121,11 @@ public class Connectors {
         }
     }
 
+    /**
+     * Prints the details of a person, including the number of messages sent, received, and team members.
+     *
+     * @param email  the email address of the person
+     */
     public void printPersonDetails(String email) {
         if (!people.contains(email)) {
             System.out.println("Email address (" + email + ") not found in the dataset.");
@@ -107,6 +141,12 @@ public class Connectors {
         System.out.println("* " + email + " is in a team with " + teamCount + " individuals");
     }
 
+    /**
+     * Returns the count of messages sent by a person.
+     *
+     * @param email  the email address of the person
+     * @return the count of messages sent
+     */
     private int getSentCount(String email) {
         int count = 0;
         for (String[] edge : edges) {
@@ -117,6 +157,12 @@ public class Connectors {
         return count;
     }
 
+    /**
+     * Returns the count of messages received by a person.
+     *
+     * @param email  the email address of the person
+     * @return the count of messages received
+     */
     private int getReceivedCount(String email) {
         int count = 0;
         for (String[] edge : edges) {
@@ -127,6 +173,12 @@ public class Connectors {
         return count;
     }
 
+    /**
+     * Returns the count of team members of a person.
+     *
+     * @param email  the email address of the person
+     * @return the count of team members
+     */
     private int getTeamCount(String email) {
         Set<String> team = new HashSet<>();
         for (String[] edge : edges) {
